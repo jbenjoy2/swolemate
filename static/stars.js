9c71ac4e03595b84d6479012394fc3605624e838
Jason Benjoya <jasonbenjoya@gmail.com>
2021-01-27 17:17:03 -0500

    added some functinality for the favorite icon and fixed navbar typo

---
+++ static/stars.js
@@ -0,0 +1,15 @@
+const $like = $('#like');
+
+function handlerIn() {
+	if ($('button#like > i').hasClass('no-like')) {
+		$like.empty();
+		$like.append('<i class="fas fa-star liked"></i>');
+	}
+}
+
+function handlerOut() {
+	$like.empty();
+	$like.append('<i class="far fa-star no-like"></i>');
+}
+
+$like.hover(handlerIn, handlerOut);
