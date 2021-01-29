1bb7b47a8ddab0457b6a14314ddbcf6a0df2034f
Jason Benjoya <jasonbenjoya@gmail.com>
2021-01-29 18:13:01 -0500

    fixed some logic errors in the public posts js code. added logic to launch register modal if there are issues

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -5,7 +5,12 @@ const $loginEmail = $('#email');
 const $loginPwd = $('#password');
 const $regEmail = $('#new_email');
 const $loginErrors = $('#login-errors');
+const $registerErrors = $('#register-errors');
 
 if ($loginErrors.text()) {
 	$loginModal.modal('show');
+}
+
+if ($registerErrors.text()) {
+	$registerModal.modal('show');
 }
